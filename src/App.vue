da32ef632fc554e8246b4b1a3379f6f40c91a79f
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-24 15:07:03 +0100

    add AppHeader, Main, Footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },
+}
 </script>
 
 <template>
